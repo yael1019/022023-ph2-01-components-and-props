a2c5213f6525a82ac5caa2fac6de430a7a6b267e
brewchetta <chett.tiller@gmail.com>
2023-03-12 19:30:30 -0400

    deleted unused files, built raccoon array

---
+++ raccoons.js
@@ -0,0 +1,22 @@
+const raccoons = [
+  {
+    id: 1,
+    name: "Robert",
+    favFood: "hamburgers"
+  },
+  {
+    id: 2,
+    name: "Rocket",
+    favFood: "pizza"
+  },
+  {
+    id: 3,
+    name: "Rocky",
+    favFood: "ice cream"
+  },
+  {
+    id: 4,
+    name: "Red",
+    favFood: "salmon"
+  },
+]
